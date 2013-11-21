a2d93f06b89c129a84db105e7183afee6c3ff1fd
BlueVM <admin@bluevm.com>
2013-11-21 00:45:53 -0700

    Patches to correct blank files.

--- feathur/template_sync.php
+++ feathur/template_sync.php
@@ -1,5 +1,6 @@
 <?php
 include('./includes/loader.php');
+session_write_close();
 
 $uTemplate = $_GET['template'];
 
@@ -8,7 +9,8 @@ if($sTemplate = $database->CachedQuery("SELECT * FROM `templates` WHERE `path` =
 	header('Content-type: application/iso');
 	header('Content-Disposition: attachment; filename="'.$sTemplate->sPath.'.iso"');
 	ob_end_clean();
-	fpassthru("/var/feathur/data/templates/kvm/{$sTemplate->sPath}.iso");
+	$sData = @fopen("/var/feathur/data/templates/kvm/{$sTemplate->sPath}.iso", "rb");
+	fpassthru($sData);
 
 } else {
 	die();
