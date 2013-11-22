af1100fed9f4b6d946ff157cb6f0a8ef7652dd33
BlueVM <admin@bluevm.com>
2013-11-21 22:43:34 -0700

    Numeric patch.

--- data.sql
+++ data.sql
@@ -139,7 +139,7 @@ INSERT INTO `settings` (`id`, `setting_name`, `setting_value`, `setting_group`) 
 (18, 'update_type', 'develop', 'site_settings'),
 (19, 'bandwidth_accounting', 'both', 'site_settings'),
 (20, 'license', '0', 'site_settings'),
-(20, 'refresh_time', '10', 'site_settings');
+(21, 'refresh_time', '10', 'site_settings');
 
 CREATE TABLE IF NOT EXISTS `statistics` (
   `id` int(16) NOT NULL AUTO_INCREMENT,
