5db2001fe321836a11c7e8590d1fc94b2400fc24
BlueVM <admin@bluevm.com>
2013-11-28 04:17:16 -0700

    Updates to create a new KVM boot system.

---
+++ Scripts/start-kvm.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Define possible types
+exists='already exists'
+missing='No such file'
+isomissing='iso'
+diskmissing='img'
+created='created from'
+
+startup=$(virsh create /var/feathur/configs/kvm$1-vps.xml 2>&1)
+ver=$(virsh --version 2>&1)
+version=${ver:0:1}
+
+if [[ "$startup" == *"$exists"* ]]
+then
+	echo 1;
+	exit 1;
+fi
+
+if [[ "$startup" == *"$missing"* ]]
+then
+	if [[ "$startup" == *"$isomissing"* ]]
+	then
+		if [ $version = "1" ];
+		then
+			if [[ "$4" == *"feathurpassword"* ]]
+			then
+				screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget $2/template_sync.php?template=$3;mv template_sync.php?template=$3 $3.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;exit;"
+			else
+				screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget $2/template_sync.php?template=$3;mv template_sync.php?template=$3 $3.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;virsh qemu-monitor-command kvm$1 --hmp change vnc :$5;virsh qemu-monitor-command kvm$1 --hmp change vnc password $4;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;exit;"
+			fi
+		else
+			screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget $2/template_sync.php?template=$3;mv template_sync.php?template=$3 $3.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;exit;"
+		fi	
+		echo 2;
+		exit 1;
+	fi
+	
+	if [[ "$startup" == *"$diskmissing"* ]]
+	then
+		echo 3;
+		exit 1;
+	fi
+fi
+
+if [[ "$startup" == *"$created"* ]]
+then
+	if [ $version = "1" ];
+	then
+		if [[ "$4" == *"feathurpassword"* ]]
+		then
+			screen -dmS balance bash -c "cd /var/feathur/data/;python /var/feathur/data/balancer.py;exit;"
+		else
+			screen -dmS balance bash -c "virsh qemu-monitor-command kvm$1 --hmp change vnc :$5;virsh qemu-monitor-command kvm$1 --hmp change vnc password $4;cd /var/feathur/data/;python /var/feathur/data/balancer.py;exit;"
+		fi
+	else
+		screen -dmS balance bash -c "cd /var/feathur/data/;python /var/feathur/data/balancer.py;exit;"
+	fi
+	echo 4;
+	exit 1;
+fi
+
+echo 5;
+exit 1;
