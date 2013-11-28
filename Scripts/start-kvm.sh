135638639a480ddd8dcf432e48647b0030494cd2
BlueVM <admin@bluevm.com>
2013-11-28 05:24:13 -0700

    Patch to prevent 'start spamming'

--- Scripts/start-kvm.sh
+++ Scripts/start-kvm.sh
@@ -8,9 +8,12 @@ diskmissing='img'
 created='created from'
 
 url=${2// /.}
+iso=${3// /.}
+isotemp="/var/feathur/data/templates/kvm/template_sync.php?template=$iso"
 startup=$(virsh create /var/feathur/configs/kvm$1-vps.xml 2>&1)
 ver=$(virsh --version 2>&1)
 version=${ver:0:1}
+
 
 if [[ "$startup" == *"$exists"* ]]
 then
@@ -22,19 +25,27 @@ if [[ "$startup" == *"$missing"* ]]
 then
 	if [[ "$startup" == *"$isomissing"* ]]
 	then
+	
+		if [ ! -f "$isotemp" ];
+		then
+			echo 2;
+			exit 1;
+		fi
+		
 		if [ $version = "1" ];
 		then
 			if [[ "$4" == *"feathurpassword"* ]]
 			then
-				screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget '$url/template_sync.php?template=$3';mv template_sync.php?template=$3 $3.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;sleep 600;"
+				screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget '$url/template_sync.php?template=$iso';mv template_sync.php?template=$iso $iso.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;sleep 600;"
 			else
-				screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget '$url/template_sync.php?template=$3';mv template_sync.php?template=$3 $3.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;virsh qemu-monitor-command kvm$1 --hmp change vnc :$5;virsh qemu-monitor-command kvm$1 --hmp change vnc password $4;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;sleep 600;"
+				screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget '$url/template_sync.php?template=$iso';mv template_sync.php?template=$iso $iso.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;virsh qemu-monitor-command kvm$1 --hmp change vnc :$5;virsh qemu-monitor-command kvm$1 --hmp change vnc password $4;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;sleep 600;"
 			fi
 		else
-			screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget '$url/template_sync.php?template=$3';mv template_sync.php?template=$3 $3.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;sleep 600;"
+			screen -dmS template bash -c "mkdir -p /var/feathur/data/templates/kvm/;cd /var/feathur/data/templates/kvm/;wget '$url/template_sync.php?template=$iso';mv template_sync.php?template=$iso $iso.iso;virsh create /var/feathur/configs/kvm$1-vps.xml;cd /var/feathur/data/templates/kvm/;rm -rf *index.html*;rm -rf *template_sync*;cd /var/feathur/data/;python /var/feathur/data/balancer.py;sleep 600;"
 		fi	
 		echo 2;
 		exit 1;
+		
 	fi
 	
 	if [[ "$startup" == *"$diskmissing"* ]]
@@ -42,6 +53,7 @@ then
 		echo 3;
 		exit 1;
 	fi
+	
 fi
 
 if [[ "$startup" == *"$created"* ]]
