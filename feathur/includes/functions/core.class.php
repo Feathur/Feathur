7de031b9709fd3837d880b9dbfccdab1ff372947
BlueVM <admin@bluevm.com>
2013-11-30 15:00:36 -0700

    Patch to fix potential settings missing.

--- feathur/includes/functions/core.class.php
+++ feathur/includes/functions/core.class.php
@@ -3,8 +3,11 @@ class Core {
 
 	static function GetSetting($setting){
 		global $database;
-		$result = $database->CachedQuery("SELECT * FROM settings WHERE `setting_name` = :Setting", array(':Setting'	=> $setting));
-		return new Setting($result);
+		if($result = $database->CachedQuery("SELECT * FROM settings WHERE `setting_name` = :Setting", array(':Setting'	=> $setting))){
+			return new Setting($result);
+		} else {
+			return false;
+		}
 	}
 	
 	static function UpdateSetting($setting, $value){
