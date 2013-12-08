f5b05157affbdb0296dd085bcbfc57f0c0bdb4fb
BlueVM <admin@bluevm.com>
2013-12-07 18:37:51 -0700

    Debugging for template sync.

--- feathur/template_sync.php
+++ feathur/template_sync.php
@@ -6,15 +6,15 @@ $uTemplate = $_GET['template'];
 
 if($sTemplate = $database->CachedQuery("SELECT * FROM `templates` WHERE `path` = :Template", array("Template" => $uTemplate))){
 	$sTemplate = new Template($sTemplate->data[0]["id"]);
-	if(!file_exists("/var/feathur/data/templates/kvm/{$sTemplate->sPath}.iso")){
-		die();
+	if(!file_exists("/var/feathur/data/templates/kvm/{$sTemplate->uPath}.iso")){
+		die("Missing template file.");
 	}
 	header('Content-type: application/iso');
-	header('Content-Disposition: attachment; filename="'.$sTemplate->sPath.'.iso"');
+	header('Content-Disposition: attachment; filename="'.$sTemplate->uPath.'.iso"');
 	ob_end_clean();
-	$sData = @fopen("/var/feathur/data/templates/kvm/{$sTemplate->sPath}.iso", "rb");
+	$sData = @fopen("/var/feathur/data/templates/kvm/{$sTemplate->uPath}.iso", "rb");
 	fpassthru($sData);
 	fclose($sData);
 } else {
-	die();
+	die("Invalid template name.");
 }
