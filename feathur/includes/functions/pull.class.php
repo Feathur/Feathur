101c51013ace908c377822f0e6d6a171c7dd2f23
BlueVM <admin@bluevm.com>
2013-12-09 23:27:04 -0700

    Debug

--- feathur/includes/functions/pull.class.php
+++ feathur/includes/functions/pull.class.php
@@ -156,10 +156,14 @@ class Pull {
 		// KVM Processing instructions
 		if($sServer->sType == 'kvm'){
 $sPullCommand = <<<COMMAND
-for i in `ip link show | grep mtu | awk '{print $2}' | awk -F: '{print $1}'`; do echo -n "$(echo $i | awk -F. '{print $1}' | awk -Fm '{print $2}') "; ifconfig $i | grep 'RX bytes' | awk -F: '{print $2,$3}' | awk '{print $1,$6}'; done > /var/feathur/data/bandwidth.tmp.txt
+for i in `ip link show | grep mtu | awk '{print $2}' | awk -F: '{print $1}'`;
+do 
+	vpsid=$(echo $i | awk -F. '{print $1}' | awk -Fm '{print $2}');
+	vpsbw=`ifconfig $i | grep 'RX bytes' | awk -F: '{print $2,$3}' | awk '{print $1,$6}';`
+	echo "$vpsid $vpsbw";
+done
 COMMAND;
-			$sIssueBandwidth = $sSSH->exec($sPullCommand);
-			$sPullBandwidth = $sSSH->exec("cat /var/feathur/data/bandwidth.tmp.txt");
+			$sPullBandwidth = $sSSH->exec($sPullCommand);
 			var_dump($sPullBandwidth);
 			foreach($sPullBandwidth as $sVPS => $sRow){
 				/*
