b89e7c497fc9773ed9a455fdb3413dd1719ca13c
BlueVM <admin@bluevm.com>
2014-03-18 03:14:54 -0600

    Patches to UserIPv6Block

--- feathur/includes/functions/useripv6blocks.class.php
+++ feathur/includes/functions/useripv6blocks.class.php
@@ -4,7 +4,7 @@ class UserIPv6Block extends CPHPDatabaseRecordClass {
 	public $table_name = "useripv6blocks";
 	public $id_field = "id";
 	public $fill_query = "SELECT * FROM useripv6blocks WHERE `id` = :Id";
-	public $verify_query = "SELECT * FROM useripv6 WHERE `id` = :Id";
+	public $verify_query = "SELECT * FROM useripv6blocks WHERE `id` = :Id";
 	public $query_cache = 1;
 	
 	public $prototype = array(
