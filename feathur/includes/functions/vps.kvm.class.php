c35d40d1617ef7674c19326095ab956826590a11
BlueVM <admin@bluevm.com>
2014-04-11 22:46:14 -0600

    Patches to KVM create.

--- feathur/includes/functions/vps.kvm.class.php
+++ feathur/includes/functions/vps.kvm.class.php
@@ -168,7 +168,7 @@ class kvm {
 					if($sSync === true){
 						$sVPS->uISOSyncing = 1;
 						$sVPS->InsertIntoDatabase();
-						$sCommandList .= "screen -dmS templatesync{$sVPS->sContainerId} bash -c \"cd /var/feathur/data/templates/kvm/;wget -O {$sTemplatePath} {$sTemplateURL}\";virsh create /var/feathur/configs/kvm{$sVPS->sContainerId}-vps.xml;virsh autostart kvm{$sVPS->sContainerId}";
+						$sCommandList .= "screen -dmS templatesync{$sVPS->sContainerId} bash -c \"cd /var/feathur/data/templates/kvm/;wget -O {$sTemplatePath} {$sTemplateURL}\";virsh create /var/feathur/configs/kvm{$sVPS->sContainerId}-vps.xml;virsh autostart kvm{$sVPS->sContainerId}\";";
 					}
 				} catch (Exception $e) {
 					$sVPS->uTemplate = 0;
