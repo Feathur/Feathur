9917dcf43aff26d2ec7e2fcf03bfe946e2b39a9d
Zach <zach@viralsec.com>
2014-05-01 14:13:21 -0400

    Cleaned up formatting and added documentation to the rest of the top-level files

--- feathur/cron.php
+++ feathur/cron.php
@@ -1,173 +1,225 @@
 <?php
-// Check to make sure that this script isn't being executed remotely.
-if(!(php_sapi_name() == 'cli')){
-	die("Unfortunately this script must be executed via CLI.");
-}
 
-// Place us in the correct directory...
-// Call prerequisites, set variables.
+/*
+ * See if we're running as CLI
+ */
+
+if(php_sapi_name() != 'cli') die('Unfortunately this script must be executed via CLI.');
+
+/*
+ * Stage execution
+ */
+
 chdir('/var/feathur/feathur/');
-include('./includes/loader.php');
+require_once('./includes/loader.php');
 error_reporting(E_ALL ^ E_NOTICE);
 $sTime = time();
 
-// Set timeout and memory limit then
-// connect to the local host.
-set_time_limit(6000);
-ini_set('memory_limit','512M');
+/*
+ * Set PHP limits
+ */
+
+@set_time_limit(6000);
+@ini_set('memory_limit','512M');
+
+/*
+ * Attempt SSH connection
+ */
+
 $sLocalSSH = new Net_SSH2('127.0.0.1');
 $sLocalKey = new Crypt_RSA();
 $sLocalKey->loadKey(file_get_contents($cphp_config->settings->rootkey));
-if(!($sLocalSSH->login("root", $sLocalKey))) {
-	die("Cannot connect to this server, check local key.");
-}
+if (! $sLocalSSH->login("root", $sLocalKey)) die("Cannot connect to this server, check local key.");
 
-// Create template link for KVM.
+/*
+ * Create template link for KVM
+ */
+
 $sLink = $sLocalSSH->exec("ln -s /var/feathur/data/templates/kvm /var/feathur/feathur/templates/");
 
-// Remove old screens
+/*
+ * Clean up screen sessions
+ */
+
 $sClean = $sLocalSSH->exec("killall --older-than 10m screen");
 
-// Check to make sure that all template urls are valid and working on a 15 minute basis.
+/*
+ * Verify templates
+ */
+
 echo "Begining template validity checks...\n";
-$sTemplateSync = Core::GetSetting('last_template_sync');
-$sBefore = time() - (60 * 15);
-$sTemplateSync = $sTemplateSync->sValue;
-$sTimestamp = time();
-if($sTemplateSync < $sBefore){
-	// Attempt to get data about the template.
-	if($sTemplateList = $database->CachedQuery("SELECT * FROM `templates` WHERE `disabled` < 2", array())){
-		foreach($sTemplateList->data as $sTemplate){
-			$sTemplate = new Template($sTemplate["id"]);
-			try {
-				$sTemplateData = array_change_key_case(get_headers($sTemplate->sURL, TRUE));
-				if((!isset($sTemplateData['content-length'])) || (empty($sTemplateData['content-length']))){
-					throw new Exception("ISO Invalid");
-				}
-			} catch (Exception $e) {
-				$sDisable = 1;
-			}
-			
-			if(($sTemplate->sDisabled == 0) && ($sDisable == 1)){
-				$sTemplate->uDisabled = 1;
-				$sTemplate->InsertIntoDatabase();
-			} else {
-				$sTemplate->uDisabled = 0;
-				$sTemplate->InsertIntoDatabase();
-			}
-			unset($sDisable);
+$sTemplateSync	= Core::GetSetting('last_template_sync');
+$sBefore		= time() - 900;
+$sTemplateSync	= $sTemplateSync->sValue;
+$sTimestamp		= time();
+
+if ($sTemplateSync < $sBefore)
+{
+
+  if ($sTemplateList = $database->CachedQuery('SELECT * FROM `templates` WHERE `disabled` < 2', array()))
+  {
+
+    foreach ($sTemplateList->data as $sTemplate)
+	{
+	  $sTemplate = new Template($sTemplate['id']);
+	
+	  try {
+		$sTemplateData = array_change_key_case(get_headers($sTemplate->sURL, TRUE));
+		if (!isset($sTemplateData['content-length']) || empty($sTemplateData['content-length']))
+		{
+		  throw new Exception("ISO {$sTemplate->sURL} is invalid\n");
 		}
+	  } catch (Exception $e) {
+		$sDisable = 1;
+	  }
+	  
+	  if (($sTemplate->sDisabled == 0) && ($sDisable == 1))
+	  {
+		$sTemplate->uDisabled = 1;
+		$sTemplate->InsertIntoDatabase();
+	  } else {
+		$sTemplate->uDisabled = 0;
+		$sTemplate->InsertIntoDatabase();
+	  }
+
+	  unset($sDisable);
 	}
-	$sUpdateTimestamp = Core::UpdateSetting('last_template_sync', time());
+  }
+  $sUpdateTimestamp = Core::UpdateSetting('last_template_sync', time());
 } else {
-	echo "Template URLs have been checked recently, skipping...\n";
+  echo "Template URLs have been checked recently, skipping...\n";
 }
 
-// System status tracker.
-if($sServerList = $database->CachedQuery("SELECT * FROM servers", array())){
-	foreach($sServerList->data as $sServer){
-		if($sTotal == 5) {
-			$sLocalSSH->exec($sCommandList);
-			unset($sCommandList);
-			unset($sTotal);
-			echo "Launched a batch of uptime checkers.\n";
-			sleep(5);
-		}
-		$sServer = new Server($sServer["id"]);
-		$sCommandList .= "screen -dm -S uptracker bash -c 'cd /var/feathur/feathur/scripts/;php pull_server.php {$sServer->sId} >> /var/feathur/data/status.log;exit;';";
-		$sTotal++;
-		
-		$sBefore = (time() - (5 * 60));
-		$sUptime = $sServer->sLastCheck;
-		$sStatus = $sServer->sStatus;
-		if(($sBefore > $sUptime) && ($sStatus === true)){
-			$sStatusWarning = $sServer->sStatusWarning;
-			if($sStatusWarning === false){
-				if($sAdminList = $database->CachedQuery("SELECT * FROM `accounts` WHERE `permissions` = :Permissions", array("Permissions" => 7))){
-					foreach($sAdminList->data as $sAdmin){
-						$sVariable = array("server" => $sServer->sName);
-						$sAlert = Core::SendEmail($sAdmin["email_address"], "Server Down: {$sServer->sName} - To {$sAdmin['email_address']}", "down", $sVariable);
-					}
-				}
-				$sServer->uStatusWarning = true;
-				echo "Sent email about outage to administrators.\n";
-			}
-			$sServer->uStatus = false;
-			$sServer->InsertIntoDatabase();
-		}
+/*
+ * Server status tracker
+ */
+
+if ($sServerList = $database->CachedQuery('SELECT * FROM servers', array()))
+{
+  foreach($sServerList->data as $sServer)
+  {
+	if ($sTotal == 5)
+	{
+	  $sLocalSSH->exec($sCommandList);
+	  unset($sCommandList);
+	  unset($sTotal);
+	  echo "Dispatched 5 uptime checkers...\n";
+	  sleep(5);
 	}
 	
-	$sLocalSSH->exec($sCommandList);
-	unset($sCommandList);
-	unset($sTotal);
-	echo "Finished launching uptime checkers.\n";
+	$sServer = new Server($sServer['id']);
+	$sCommandList .= "screen -dm -S uptracker bash -c 'cd /var/feathur/feathur/scripts/;php pull_server.php {$sServer->sId} >> /var/feathur/data/status.log;exit;';";
+	$sTotal++;
+		
+	$sBefore = time() - 300;
+	$sUptime = $sServer->sLastCheck;
+	$sStatus = $sServer->sStatus;
+	if (($sBefore > $sUptime) && ($sStatus === true))
+	{
+	  $sStatusWarning = $sServer->sStatusWarning;
+	  if ($sStatusWarning === false)
+	  {
+		if ($sAdminList = $database->CachedQuery('SELECT * FROM `accounts` WHERE `permissions` = :Permissions', array('Permissions' => 7)))
+		{
+		  foreach ($sAdminList->data as $sAdmin)
+		  {
+			$sVariable = array('server' => $sServer->sName);
+			$sAlert = Core::SendEmail($sAdmin['email_address'], "Server Down: {$sServer->sName} - To {$sAdmin['email_address']}", 'down', $sVariable);
+		  }
+		}
+		$sServer->uStatusWarning = true;
+		echo "Dispatched outage notification email for Server {$sServer->sName}\n";
+	  }
+	  $sServer->uStatus = false;
+	  $sServer->InsertIntoDatabase();
+	}
+  }
+
+  $sLocalSSH->exec($sCommandList);
+  unset($sCommandList);
+  unset($sTotal);
+  echo "Done\n";
 }
 
-// Cleanup old statistics...
-$sOldStatistics = (time() - (60*60*24*5));
-$sStatistics = $database->prepare("DELETE FROM `statistics` WHERE timestamp < :OldStatistics");
+/*
+ * Clean up old statistics
+ */
+
+$sOldStatistics = time() - 432000;
+$sStatistics = $database->prepare('DELETE FROM `statistics` WHERE timestamp < :OldStatistics');
 $sStatistics->bindParam(':OldStatistics', $sOldStatistics, PDO::PARAM_INT);  
 $sStatistics->execute();
 
-// Cleanup old history...
-$sOldHistory = (time() - (60*60*24*7));
-$sHistory = $database->prepare("DELETE FROM `history` WHERE timestamp < :OldHistory");
+/*
+ * Clean up old history
+ */
+
+$sOldHistory = time() - 604800;
+$sHistory = $database->prepare('DELETE FROM `history` WHERE timestamp < :OldHistory');
 $sHistory->bindParam(':OldHistory', $sOldHistory, PDO::PARAM_INT);  
 $sHistory->execute();
 
-// Reset bandwidth if today is the first day of the month and the last reset was more than 7 days ago.
-$sLastReset = Core::GetSetting('bandwidth_timestamp');
-$sTimeAgo = (time() - (((60 * 60) * 24) * 7));
-$sDayToday = date('j');
-if(($sLastReset->sValue < $sTimeAgo) && ($sDayToday == 1)){
-	$sReset = $database->prepare("UPDATE `vps` SET `bandwidth_usage` = '0'");
-	$sReset->execute();
-	$sUpdateReset = Core::UpdateSetting('bandwidth_timestamp', time());
+/*
+ * Reset bandwidth if today is the first day of the month, and the last reset was >7 days ago
+ */
+
+$sLastReset	= Core::GetSetting('bandwidth_timestamp');
+$sTimeAgo	= time() - 604800;
+$sDayToday	= date('j');
+if (($sLastReset->sValue < $sTimeAgo) && ($sDayToday == 1))
+{
+  $sReset = $database->prepare('UPDATE `vps` SET `bandwidth_usage` = :Zero');
+  $sReset->bindParam(':Zero', 0, PDO::PARAM_INT);
+  $sReset->execute();
+  $sUpdateReset = Core::UpdateSetting('bandwidth_timestamp', time());
 }
 
-// License
-// Please don't remove or edit this code, a lot of work went into Feathur.
-// Thank us for our work by leaving this code here or by paying for a license.
-// While I realize this won't stop anyone who really wants to disable the "alert" system, it might prevent someone who knows nothing about PHP.
-echo "License update...";
-if($sSlaves = $database->CachedQuery("SELECT * FROM servers", array())){
-	$sCountSlaves = count($sSlaves->data);
-}
-$sHost = Core::GetSetting('panel_url');
-$sURL = "http://check.feathur.com/api.php?host={$sHost->sValue}&slaves={$sCountSlaves}";
-$sCurl = curl_init();
+/*
+ * License Check
+ */
+
+echo "License update...\n";
+if ($sSlaves = $database->CachedQuery('SELECT * FROM servers', array())) $sCountSlaves = count($sSlaves->data);
+$sHost	= Core::GetSetting('panel_url');
+$sURL	= "http://check.feathur.com/api.php?host={$sHost->sValue}&slaves={$sCountSlaves}";
+$sCurl	= curl_init();
 curl_setopt($sCurl, CURLOPT_URL, $sURL);
 curl_setopt($sCurl, CURLOPT_RETURNTRANSFER, 1);
 $sLicense = json_decode(curl_exec($sCurl), true);
 curl_close($sCurl);
-if($sLicense["type"] == 'success'){
-	$sUpdateLicense = Core::UpdateSetting('license', "1");
-} else {
-	$sUpdateLicense = Core::UpdateSetting('license', "0");
-}
+Core::UpdateSetting('license', ($sLicense['type']=='success' ? 1 : 0));
 
 
-// Check for updates.
-echo "Checking for updates if available...";
-$sAutomaticUpdates = Core::GetSetting('automatic_updates');
-$sAutomaticUpdates = $sAutomaticUpdates->sValue;
-$sLastUpdateCheck = Core::GetSetting('last_update_check');
-$sLastUpdateCheck = $sLastUpdateCheck->sValue;
-$sTimeAgo = time - (15 * 60);
-if($sLastUpdateCheck < $sTimeAgo){
-	if($sAutomaticUpdates == 1){
-		$sSSH = new Net_SSH2('127.0.0.1');
-		$sKey = new Crypt_RSA();
-		$sKey->loadKey(file_get_contents($cphp_config->settings->rootkey));
-		if($sSSH->login("root", $sKey)) {
-			$sSSH->exec("cd /var/feathur/; git pull; cd /var/feathur/feathur/; php update.php; rm -rf update.php;");
-			$sVersion = $sSSH->exec("cat /var/feathur/version.txt");
-			$sLastUpdate = Core::UpdateSetting('last_update_check', time());
-			$sNewVersion = Core::UpdateSetting('current_version', $sVersion);
-		}
+/*
+ * Update check
+ */
+
+echo "Dispatching update check...\n";
+$sAutomaticUpdates	= Core::GetSetting('automatic_updates');
+$sAutomaticUpdates	= $sAutomaticUpdates->sValue;
+$sLastUpdateCheck	= Core::GetSetting('last_update_check');
+$sLastUpdateCheck	= $sLastUpdateCheck->sValue;
+$sTimeAgo			= time - 900;
+if ($sLastUpdateCheck < $sTimeAgo)
+{
+  if ($sAutomaticUpdates == 1)
+  {
+	$sSSH = new Net_SSH2('127.0.0.1');
+	$sKey = new Crypt_RSA();
+	$sKey->loadKey(file_get_contents($cphp_config->settings->rootkey));
+	if ($sSSH->login("root", $sKey))
+	{
+	  $sSSH->exec('cd /var/feathur/; git pull; cd /var/feathur/feathur/; php -f /var/feathur/feathur/update.php; rm -f /var/feathur/feathur/update.php');
+	  $sVersion = $sSSH->exec('cat /var/feathur/version.txt');
+	  $sLastUpdate = Core::UpdateSetting('last_update_check', time());
+	  $sNewVersion = Core::UpdateSetting('current_version', $sVersion);
 	}
+  }
 }
 
-// Release cron locks.
+/*
+ * Release lock files
+ */
+
 $sLock = $sLocalSSH->exec("rm -rf /var/feathur/data/bandwidth.lock");
 
--- feathur/view.php
+++ feathur/view.php
@@ -1,88 +1,167 @@
 <?php
-include('./includes/loader.php');
+require_once('./includes/loader.php');
 
-// Check for login.
-if(empty($sUser)){
-	header("Location: index.php");
-	die();
-}
+/*
+ * Redirect user to main page if not logged in
+ */
 
-// Check to make sure the user is actually trying to view a vps.
-if((empty($sId)) || (!is_numeric($sId))){
-	header("Location: main.php");
-	die();
-}
+if (!empty($sUser)) die(header("Location: main.php", 401));
 
-// Check to see if the user owns the VPS or if the VPS exists at all (if admin).
+
+/*
+ * Redirect to main page if user is not looking at a VPS
+ */
+
+if ((empty($sId)) || (!is_numeric($sId))) die(header("Location: main.php", 403));
+
+$sAction	= preg_replace('/[^\w\d]/', '', $_GET['action']);
+
+/*
+ * Check for the existence of a VPS for currently logged in user, or existence at all if admin
+ */
+
 try {
-	if($sUser->sPermissions != 7){
-		$database->CachedQuery("SELECT * FROM vps WHERE `id` = :VPSId AND `user_id` = :UserId", array('VPSId' => $sId, 'UserId' => $sUser->sId));
+	if ($sUser->sPermissions != 7)
+	{
+	  $database->CachedQuery('SELECT * FROM vps WHERE `id` = :VPSId AND `user_id` = :UserId', array('VPSId' => $sId, 'UserId' => $sUser->sId));
 	} else {
-		$database->CachedQuery("SELECT * FROM vps WHERE `id` = :VPSId", array('VPSId' => $sId));
+	  $database->CachedQuery('SELECT * FROM vps WHERE `id` = :VPSId', array('VPSId' => $sId));
 	}
-	
 	$sVPS = new VPS($sId);
-	
 } catch (Exception $e) {
-	header("Location: main.php");
-	die();
+	die(header("Location: main.php", 401));
 }
 
-if(($sVPS->sUserId != $sUser->sId) && ($sUser->sPermissions != 7)){
-	header("Location: main.php");
-	die();
-}
+/*
+ * Redirect if currently logged in user does not own the VPS, and user is not an admin
+ */
 
-// Restrict access to the vps if the user's vps is suspended.
+if (($sVPS->sUserId != $sUser->sId) && ($sUser->sPermissions != 7)) die(header("Location: main.php", 401));
+
+/*
+ * Restrict access to VPS management if VPS status is currently suspended
+ */
+
 $sSuspended = $sVPS->sSuspended;
-if((!empty($sSuspended)) && ($sUser->sPermissions != 7)){
-	if($sSuspended == 2){
-		$sUserView = Templater::AdvancedParse($sTemplate->sValue.'/abuse', $locale->strings, array("Hostname" => $sVPS->sHostname));
-	} else {
-		$sUserView = Templater::AdvancedParse($sTemplate->sValue.'/suspended', $locale->strings, array("Hostname" => $sVPS->sHostname));
-	} 
-	echo Templater::AdvancedParse($sTemplate->sValue.'/master', $locale->strings, array("Content" => $sUserView, "Page" => "main"));
+if ((!empty($sSuspended)) && ($sUser->sPermissions != 7))
+{
+  if ($sSuspended == 2)
+  {
+	$sUserView = Templater::AdvancedParse(
+	               $sTemplate->sValue.'/abuse',
+				   $locale->strings,
+				   array('Hostname' => $sVPS->sHostname)
+				 );
+  } else {
+	$sUserView = Templater::AdvancedParse(
+	               $sTemplate->sValue.'/suspended',
+				   $locale->strings,
+				   array('Hostname' => $sVPS->sHostname)
+				 );
+  } 
+  echo Templater::AdvancedParse(
+         $sTemplate->sValue.'/master',
+		 $locale->strings,
+		 array(
+		   'Content'	=> $sUserView,
+		   'Page'		=> 'main'
+		 )
+	   );
+  die();
+}
+
+/*
+ * Restrict access to VPS management if VPS is currently being transferred
+ */
+
+if ($sTransfer = $database->CachedQuery('SELECT * FROM transfers WHERE `completed` = 0 AND `vps_id` = :VPSId', array('VPSId' => $sVPS->sId)))
+{
+  $sUserView = Templater::AdvancedParse(
+                 $sTemplate->sValue.'/transfer',
+				 $locale->strings,
+				 array()
+			   );
+	echo Templater::AdvancedParse(
+	       $sTemplate->sValue.'/master',
+		   $locale->strings,
+		   array(
+		     'Content'	=> $sUserView,
+			 'Page'		=> 'main',
+			 'Errors'	=> $sErrors,
+			 'VPSId'	=> $sVPS->sId
+		   )
+		 );
 	die();
 }
 
-// Restrict access to the vps if the VPS is being transferred.
-if($sTransfer = $database->CachedQuery("SELECT * FROM transfers WHERE `completed` = 0 AND `vps_id` = :VPSId", array('VPSId' => $sVPS->sId))){
-	$sUserView = Templater::AdvancedParse($sTemplate->sValue.'/transfer', $locale->strings, array());
-	echo Templater::AdvancedParse($sTemplate->sValue.'/master', $locale->strings, array("Content" => $sUserView, "Page" => "main", "Errors" => $sErrors, "VPSId" => $sVPS->sId));
-	die();
+/*
+ * Process any actions given
+ */
+
+if (!empty($sAction))
+{
+  @set_time_limit(100);
+  $sStart = new $sVPS->sType;
+  $sDBAction = "database_{$sVPS->sType}_{$sAction}";
+  $sServerAction = "{$sVPS->sType}_{$sAction}";
+  if (((method_exists($sStart, $sDBAction) === true)) && ((method_exists($sStart, $sServerAction) === true)))
+  {
+    $sDBResult = $sStart->$sDBAction($sUser, $sVPS, $sRequested);
+    if (is_array($sDBResult)) die(json_encode($sDBResult));
+	$sServerResult = $sStart->$sServerAction($sUser, $sVPS, $sRequested);
+	if (!empty($sServerResult['json'])) die(json_encode($sServerResult));
+  } else {
+	die(json_encode(array('json' => 1, 'type' => 'error', 'result' => 'Invalid action requested. Please try again.', 'reload' => 1)));
+  }
 }
 
-if(!empty($sAction)){
-	set_time_limit(100);
-	$sStart = new $sVPS->sType;
-	$sDBAction = "database_{$sVPS->sType}_{$sAction}";
-	$sServerAction = "{$sVPS->sType}_{$sAction}";
-	if(((method_exists($sStart, $sDBAction) === true)) && ((method_exists($sStart, $sServerAction) === true))){
-		$sDBResult = $sStart->$sDBAction($sUser, $sVPS, $sRequested);
-		if(is_array($sDBResult)){
-			echo json_encode($sDBResult);
-			die();
-		}
-		$sServerResult = $sStart->$sServerAction($sUser, $sVPS, $sRequested);
-		if(!empty($sServerResult["json"])){
-			echo json_encode($sServerResult);
-			die();
-		}
-	} else {
-		echo json_encode(array("json" => 1, "type" => "error", "result" => "Invalid action requested. Please try again.", "reload" => 1));
-		die();
-	}
+/*
+ * Ensure VPS isn't currently rebuilding
+ */
+
+if ($sVPS->sRebuilding == 1)
+{
+  $sUserView .= Templater::AdvancedParse(
+                  $sTemplate->sValue.'/rebuild',
+				  $locale->strings,
+				  array('VPS' => array('data' => $sVPS->uData))
+				);
+  echo Templater::AdvancedParse(
+         $sTemplate->sValue.'/master',
+		 $locale->strings,
+		 array('Content' => $sUserView, 'Page' => 'main')
+	   );
+  die();
 }
 
-// Check to make sure VPS isn't rebuilding.
-if($sVPS->sRebuilding == 1){
-	$sUserView .= Templater::AdvancedParse($sTemplate->sValue.'/rebuild', $locale->strings, array("VPS" => array("data" => $sVPS->uData)));
-	echo Templater::AdvancedParse($sTemplate->sValue.'/master', $locale->strings, array("Content" => $sUserView, "Page" => "main"));
-	die();
-}
+/*
+ * Pull VPS owner data
+ */
 
 $sVPSOwner = new User($sVPS->sUserId);
 
-$sUserView .= Templater::AdvancedParse($sTemplate->sValue.'/'.$sVPS->sType.'.view', $locale->strings, array("VPS" => array("data" => $sVPS->uData), "IPs" => VPS::list_ipspace($sVPS), "Templates" => VPS::list_templates($sVPS), "Servers" => VPS::list_servers($sVPS), "User" => array("data" => $sVPSOwner->uData), "UserVPSList" => VPS::list_uservps($sVPS), "IPv6Exist" => Block::ipv6_exist($sVPS), "UserIPv6Block" => Block::vps_ipv6_block($sVPS)));
-echo Templater::AdvancedParse($sTemplate->sValue.'/master', $locale->strings, array("Content" => $sUserView, "Page" => "main"));
+$sUserView .= Templater::AdvancedParse(
+                $sTemplate->sValue.'/'.$sVPS->sType.'.view',
+				$locale->strings,
+				array(
+				  'VPS'				=> array('data' => $sVPS->uData),
+				  'IPs' 			=> VPS::list_ipspace($sVPS),
+				  'Templates'		=> VPS::list_templates($sVPS),
+				  'Servers'			=> VPS::list_servers($sVPS),
+				  'User'			=> array('data' => $sVPSOwner->uData),
+				  'UserVPSList'		=> VPS::list_uservps($sVPS),
+				  'IPv6Exist'		=> Block::ipv6_exist($sVPS),
+				  'UserIPv6Block'	=> Block::vps_ipv6_block($sVPS)
+				)
+			  );
+
+/*
+ * Display user view template
+ */
+
+echo Templater::AdvancedParse(
+       $sTemplate->sValue.'/master',
+	   $locale->strings,
+	   array('Content' => $sUserView, 'Page' => 'main')
+	 );
 die();
