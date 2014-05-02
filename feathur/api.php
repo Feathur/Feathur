a8d918db8a14b0e18416aad8104f68d54c526c34
BlueVM <admin@bluevm.com>
2014-05-02 11:45:30 -0600

    Patches to fix email bug in API.

--- feathur/api.php
+++ feathur/api.php
@@ -14,7 +14,7 @@ if (!empty($_POST['email']) && !empty($_POST['password']))
 }
 
 $sAction = preg_replace('/[^\w\d]/', '', $_POST['action']);
-$sEmail  = preg_replace('/[^\w\d_\._@+]/', '', $_POST['useremail']);
+$sEmail  = preg_replace('/[^\w\d_\-\._@+]/', '', $_POST['useremail']);
 $sServer = preg_replace('/[^\d\w\.\-_]/', '', $_POST['server']);
 $sTemplate = preg_replace('/[^\w\d\s\-\._\(\)]/', '', $_POST['template']);
 $iVPSid  = abs((int) $_POST['vpsid']);
