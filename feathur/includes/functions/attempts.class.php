12ab3a1b589cc305beeee892a8edb0a1c2841e2c
Zach <zeadie@gmail.com>
2014-08-25 14:25:18 -0400

    Update attempts.class.php
    Update for single-quotes and formatting changes

--- feathur/includes/functions/attempts.class.php
+++ feathur/includes/functions/attempts.class.php
@@ -1,19 +1,22 @@
 <?php
-class Attempt extends CPHPDatabaseRecordClass {
 
-	public $table_name = "attempts";
-	public $id_field = "id";
-	public $fill_query = "SELECT * FROM attempts WHERE `id` = :Id";
-	public $verify_query = "SELECT * FROM attempts WHERE `id` = :Id";
-	public $query_cache = 1;
-	
-	public $prototype = array(
-		'numeric' => array(
-			"Timestamp" => "timestamp",
-		),
-		'string' => array(
-			'IPAddress' => "ip_address",
-			'Type' => "type",
-		),
-	);
+class Attempt extends CPHPDatabaseRecordClass
+{
+
+  public $table_name = 'attempts';
+  public $id_field = 'id';
+  public $fill_query = 'SELECT * FROM attempts WHERE `id` = :Id';
+  public $verify_query = 'SELECT * FROM attempts WHERE `id` = :Id';
+  public $query_cache = 1;
+
+  public $prototype = array(
+    'numeric' => array(
+      'Timestamp' => 'timestamp',
+    ),
+    'string' => array(
+      'IPAddress' => 'ip_address',
+      'Type' => 'type',
+    )
+  );
+
 }
