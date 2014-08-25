19d40046144a22b2754d69233bcf653ead926fb9
Zach <zeadie@gmail.com>
2014-08-25 15:36:43 -0400

    Update groups.class.php
    Formatting changes

--- feathur/includes/functions/groups.class.php
+++ feathur/includes/functions/groups.class.php
@@ -1,24 +1,33 @@
 <?php
-class Group extends CPHPDatabaseRecordClass {
 
-	public $table_name = "groups";
-	public $id_field = "id";
-	public $fill_query = "SELECT * FROM groups WHERE `id` = :Id";
-	public $verify_query = "SELECT * FROM groups WHERE `id` = :Id";
-	public $query_cache = 1;
-	
-	public $prototype = array(
-		'string' => array(
-			'Name' => "name",
-		),
-	);
-	
-	public static function array_groups(){
-		global $database;
-		$sGroups = $database->CachedQuery("SELECT * FROM groups", array());
-		foreach($sGroups->data as $value){
-			$sGroupList[] = array("id" => $value["id"], "name" => $value["name"]);
-		}
-		return $sGroupList;
-	}
+class Group extends CPHPDatabaseRecordClass
+{
+
+  public $table_name = 'groups';
+  public $id_field = 'id';
+  public $fill_query = 'SELECT * FROM groups WHERE `id` = :Id';
+  public $verify_query = 'SELECT * FROM groups WHERE `id` = :Id';
+  public $query_cache = 1;
+
+  public $prototype = array(
+    'string' => array(
+      'Name' => 'name'
+    )
+  );
+
+  public static function array_groups()
+  {
+
+    global $database;
+
+    $sGroups = $database->CachedQuery('SELECT * FROM groups', array());
+
+    foreach($sGroups->data as $value)
+    {
+      $sGroupList[] = array('id' => $value['id'], 'name' => $value['name']);
+    }
+
+    return $sGroupList;
+  }
+
 }
