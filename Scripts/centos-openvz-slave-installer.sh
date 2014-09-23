9aec2b00b44e402ced0327ea18e994d90d7d73a1
linuxthefish <linuxthefish@users.noreply.github.com>
2014-09-23 13:02:47 +0100

    Update centos-openvz-slave-installer.sh
    Added ploop to the list of packages that needs to be installed, as VPS's fail to create without it on modern openvz kernels!

--- Scripts/centos-openvz-slave-installer.sh
+++ Scripts/centos-openvz-slave-installer.sh
@@ -3,7 +3,7 @@ curl http://download.openvz.org/openvz.repo > /etc/yum.repos.d/openvz.repo
 yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/sysconfig/selinux
 setenforce 0
-yum -y install pigz screen vim vzkernel vzctl rsync
+yum -y install pigz screen vim vzkernel vzctl rsync ploop
 echo "SELINUX=disabled" > /etc/sysconfig/selinux
 echo "SELINUXTYPE=targeted" >> /etc/sysconfig/selinux
 cd /
