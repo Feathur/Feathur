717b6d875a58bd6b82abfb8f9063ce48687f57c2
Feathur <admin@feathur.com>
2015-07-05 14:14:20 -0400

    Let's make this work with PHP 5.3 and be as secure as possible for now.

--- feathur/cphp/include.misc.php
+++ feathur/cphp/include.misc.php
@@ -15,12 +15,29 @@ if($_CPHP !== true) { die(); }
 
 function random_string($length)
 {
-	$output = "";
-	for ($i = 0; $i < $length; $i++) 
-	{ 
-		$output .= substr("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", random_int(0, 61), 1); 
+	$pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+	$crypto_rand_secure = function ( $min, $max ) {
+		$range = $max - $min;
+		if ( $range < 0 ) return $min; // not so random...
+		$log    = log( $range, 2 );
+		$bytes  = (int) ( $log / 8 ) + 1; // length in bytes
+		$bits   = (int) $log + 1; // length in bits
+		$filter = (int) ( 1 << $bits ) - 1; // set all lower bits to 1
+		do {
+			$rnd = hexdec( bin2hex( openssl_random_pseudo_bytes( $bytes ) ) );
+			$rnd = $rnd & $filter; // discard irrelevant bits
+		} while ( $rnd >= $range );
+		return $min + $rnd;
+	};
+
+	$token = "";
+	$max   = strlen( $pool );
+	for ( $i = 0; $i < $length; $i++ ) {
+		$token .= $pool[$crypto_rand_secure( 0, $max )];
 	}
-	return $output;
+	
+	return $token;
 }
 
 function extract_globals()
